edc27efbf7e1046c6908916782c7d13e9cf815e3
your Name <mohamedabdulayubm@gmail.com>
2022-06-21 20:13:29 +0530

    preabs

--- Day9_Emp/Program.cs
+++ Day9_Emp/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Welcome to Employee Wage Computation Program");
+int empPresent = 1;
+Random num = new Random();
+int attendance = num.Next(0, 2);
+if (attendance == empPresent)
+{
+
+
+    Console.WriteLine("Is Present");
+
+
+}
+
+else
+{
+    Console.WriteLine("Is Absent");
+}
