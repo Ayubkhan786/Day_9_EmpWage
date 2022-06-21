f2c9aed6b9f9cf94d2ca12436c508daf91d1e5b6
your Name <mohamedabdulayubm@gmail.com>
2022-06-21 20:09:48 +0530

    class

--- Day9_Emp/Program.cs
+++ Day9_Emp/Program.cs
@@ -1,45 +1,58 @@
-﻿Console.WriteLine("Welcome to Employee Wage Computation Program");
-int empAbsent = 0;
-int empPresent = 1;
-int empPartTime = 2;
-
-int empWrkHrs = 0;
-int empSalary = 20;
-int MAX_DAYS = 20;
-int MAX_HRS = 100;
-int empHrs = 0, maxDays = 0;
-
-while (empWrkHrs <= MAX_HRS && maxDays < MAX_DAYS)
+﻿public class Salary
 {
-    maxDays++;
+    public const int empAbsent = 0;
+    public const int empPresent = 1;
+    public const int empPartTime = 2;
 
-    Random num = new Random();
-    int attendance = num.Next(0, 3);
+    public const int empSalary = 20;
+    public const int MAX_DAYS = 20;
+    public const int MAX_HRS = 100;
+    //Console.WriteLine("Welcome to Employee Wage Computation Program");
 
-    switch (attendance)
+    public void EmployeeWage()
     {
-        case 0:
-            Console.WriteLine("Is Absent");
-            empWrkHrs = 0;
-            break;
-        case 1:
-            Console.WriteLine("Is Present");
-            empWrkHrs = 8;
-            break;
-        case 2:
-            Console.WriteLine("Is PartTime");
-            empWrkHrs = 4;
-            break;
+
+
+        int empHrs = 0, maxDays = 0, empWrkHrs = 0;
+
+        while (empWrkHrs <= MAX_HRS && maxDays < MAX_DAYS)
+        {
+            maxDays++;
+
+            Random num = new Random();
+            int attendance = num.Next(0, 3);
+
+            switch (attendance)
+            {
+                case 0:
+                    Console.WriteLine("Is Absent");
+                    empWrkHrs = 0;
+                    break;
+                case 1:
+                    Console.WriteLine("Is Present");
+                    empWrkHrs = 8;
+                    break;
+                case 2:
+                    Console.WriteLine("Is PartTime");
+                    empWrkHrs = 4;
+                    break;
+            }
+            empWrkHrs += empHrs;
+            Console.WriteLine("Day : " + maxDays + "\n " + "Hours : " + empWrkHrs);
+
+
+            int empWage = (empWrkHrs * empSalary);
+
+            Console.WriteLine("Per Day salary is :" + empWage);
+
+        }
+
+        int monthlyWage = (empWrkHrs * MAX_DAYS * empSalary);
+        Console.WriteLine("The Monthly salary is :" + monthlyWage);
     }
-    empWrkHrs += empHrs;
-    Console.WriteLine("Day : " + maxDays + "\n " + "Hours : " + empWrkHrs);
-
-
-    int empWage = (empWrkHrs * empSalary);
-
-    Console.WriteLine("Per Day salary is :" + empWage);
-
+    static void Main(String[] args)
+    {
+        Salary salary = new Salary();
+        salary.EmployeeWage();
+    }
 }
-
-int monthlyWage = (empWrkHrs * MAX_DAYS * empSalary);
-Console.WriteLine("The Monthly salary is :" + monthlyWage);
