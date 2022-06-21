bfc8c3ded200207668952807257284ee2ac3de1e
your Name <mohamedabdulayubm@gmail.com>
2022-06-21 20:16:07 +0530

    EmpWage

--- Day9_Emp/Program.cs
+++ Day9_Emp/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Welcome to Employee Wage Computation Program");
+
+int empPresent = 1;
+int empWrkHrs = 0;
+int empSalary = 50;
+Random num = new Random();
+int attendance = num.Next(0, 2);
+if (attendance == empPresent)
+{
+    Console.WriteLine("Is Present");
+    empWrkHrs = 8;
+    empSalary = 50;
+}
+
+else
+{
+    Console.WriteLine("Is Absent");
+    empWrkHrs = 0;
+    empSalary = 0;
+}
+
+int empWage = (empWrkHrs * empSalary);
+Console.WriteLine("The total salary is :" + empWage);
