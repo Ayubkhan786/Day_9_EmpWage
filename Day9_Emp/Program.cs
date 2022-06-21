35f77fe22282326c42582ec3dab4ee888cb6bf14
your Name <mohamedabdulayubm@gmail.com>
2022-06-21 20:02:25 +0530

    part

--- Day9_Emp/Program.cs
+++ Day9_Emp/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Welcome to Employee Wage Computation Program");
+
+int empAbsent = 0;
+int empPresent = 1;
+int empPartTime = 2;
+
+int empWrkHrs = 0;
+int empSalary = 50;
+
+Random num = new Random();
+int attendance = num.Next(0, 3);
+if (attendance == empPresent)
+{
+    Console.WriteLine("Is Present");
+    empWrkHrs = 8;
+    empSalary = 50;
+}
+
+else if (attendance == empAbsent)
+{
+    Console.WriteLine("Is Absent");
+    empWrkHrs = 0;
+    empSalary = 0;
+}
+else
+{
+    Console.WriteLine("Is PartTime");
+    empWrkHrs = 4;
+    empSalary = 50;
+}
+
+int empWage = (empWrkHrs * empSalary);
+Console.WriteLine("The total salary is :" + empWage);
